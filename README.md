8075645e1e28a2788b591e21801eb40d0510f709
snw380 <ty.morgan818@gmail.com>
2020-08-27 16:02:02 -0400

    yup

--- README.md
+++
@@ -1,2 +0,0 @@
-# teamProfileGenerator
-A Node CLI that takes in information about employees and generates an HTML web page that displays summaries for each person
--- index.js
+++
@@ -1,8 +0,0 @@
-const App = require("./lib/App");
-const inquirer = require("inquirer");
-
-// Initialize a new Game object
-const app = new App();
-
-// Start playing
-app.start();
